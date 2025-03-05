336eb622244e91a17db95ca826fbec7cef0a9cd5
suman <aakriti062003@gmail.com>
2025-03-05 14:28:43 +0530

    added

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-require("dotenv/config");
-
-const generateToken = (id) => {
-  const token = jwt.sign({ id: id }, process.env.Secret_Code);
-  return token;
-};
-
-module.exports = generateToken;
