27f378ec3c6c1909a682bfc626dc2a98083da5b8
Abhishek Agrawal <ashu.agrawalksj@gmail.com>
2025-05-06 15:26:42 +0530

    updated

--- controllers/emailCampaign.controller.js
+++ controllers/emailCampaign.controller.js
@@ -1,11 +1,27 @@
 // 📁 controllers/emailCampaign.controller.js
 const service = require('../services/emailCampaign.service');
+const { sendMails } = require('../controllers/mail.controller.js');
 
 exports.create = async (req, res, next) => {
   try {
-    
+
     const campaign = await service.createEmailCampaign(req.user.id, req.body);
-    res.status(201).json(campaign);
+    // Automatically send emails after creation
+    const result = await sendMails({
+      campaignId: campaign._id,
+      userId,
+      selectedEmail: req.body.selectedEmail
+    });
+    res.status(201).json({
+      success: true,
+      message: 'Email campaign created and emails sent',
+      campaign,
+      sentStats: {
+        successCount: result.successCount,
+        failCount: result.failCount
+      },
+      details: result.results
+    });
   } catch (err) {
     next(err);
   }
